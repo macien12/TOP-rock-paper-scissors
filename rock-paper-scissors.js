c29337ae405b549e28d8fb7e533e1691f20f8846
macien12 <160124196+macien12@users.noreply.github.com>
2026-05-10 11:43:30 +0200

    Function getHumanChoice, new variables

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,10 @@
-// console.log("Hello World");
+// Step 1: console.log("Hello World");
 
-/* 
+/*  
+    Step 2:
     1. Create function getComputerChoice
     2. Returng Random strings: rock, paper, scissors
-    3. Use Math.random to return one of the strings
+    3. Use Math.random to return one of the strings, it always generates number between 0 and 1, I need to make function about it!
 */
 
 function getComputerChoice () {
@@ -19,4 +20,35 @@ function getComputerChoice () {
 
 }
 
-console.log (getComputerChoice())
+
+
+
+/*
+    Step 3:
+    1. Create function getHumanChoice
+    2. Player inputs is paper, rock, scissors
+    3. Declare variable
+*/
+
+function getHumanChoice() {
+
+    let HumanInput = prompt("Rock, Paper, Scissors?");
+
+
+}
+
+// console.log (getHumanChoice())
+
+/* 
+    Step 4:
+    1. Two new variables humanScore and computerScore
+    2. They must have value of 0
+*/
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+/* 
+    
+*/
